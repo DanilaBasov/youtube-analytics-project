0c7ae970427fba36d1444c61414e1933df69544f
danilabasov <danilabasov98@gmail.com>
2023-06-01 19:21:27 +0300

    add test_

--- test_channel/test_channel.py
+++ test_channel/test_channel.py
@@ -1,55 +1,43 @@
 from src.channel import Channel
 
-moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
 def test_print_info():
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
-    assert moscowpython.print_info() == {
-  "kind": "youtube#channelListResponse",
-  "etag": "271grJ8hNCh7MtMrY5vgbqBM9ks",
-  "pageInfo": {
-    "totalResults": 1,
-    "resultsPerPage": 5
-  },
-  "items": [
-    {
-      "kind": "youtube#channel",
-      "etag": "AoNJLKi85KxHhLdeei-tLjmOKd4",
-      "id": "UC-OVMPlMA3-YCIeg4z5z23A",
-      "snippet": {
-        "title": "MoscowPython",
-        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
-        "customUrl": "@moscowdjangoru",
-        "publishedAt": "2012-07-13T09:48:44Z",
-        "thumbnails": {
-          "default": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj",
-            "width": 88,
-            "height": 88
-          },
-          "medium": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s240-c-k-c0x00ffffff-no-rj",
-            "width": 240,
-            "height": 240
-          },
-          "high": {
-            "url": "https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s800-c-k-c0x00ffffff-no-rj",
-            "width": 800,
-            "height": 800
-          }
-        },
-        "localized": {
-          "title": "MoscowPython",
-          "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
-        },
-        "country": "RU"
-      },
-      "statistics": {
-        "viewCount": "2316562",
-        "subscriberCount": "26000",
-        "hiddenSubscriberCount": false,
-        "videoCount": "686"
-      }
-    }
-  ]
-}
+    assert moscowpython.print_info() == {'etag': '271grJ8hNCh7MtMrY5vgbqBM9ks',
+     'items': [{'etag': 'AoNJLKi85KxHhLdeei-tLjmOKd4',
+                'id': 'UC-OVMPlMA3-YCIeg4z5z23A',
+                'kind': 'youtube#channel',
+                'snippet': {'country': 'RU',
+                            'customUrl': '@moscowdjangoru',
+                            'description': 'Видеозаписи со встреч питонистов и '
+                                           'джангистов в Москве и не только. :)\n'
+                                           'Присоединяйтесь: '
+                                           'https://www.facebook.com/groups/MoscowDjango! '
+                                           ':)',
+                            'localized': {'description': 'Видеозаписи со встреч '
+                                                         'питонистов и джангистов '
+                                                         'в Москве и не только. '
+                                                         ':)\n'
+                                                         'Присоединяйтесь: '
+                                                         'https://www.facebook.com/groups/MoscowDjango! '
+                                                         ':)',
+                                          'title': 'MoscowPython'},
+                            'publishedAt': '2012-07-13T09:48:44Z',
+                            'thumbnails': {'default': {'height': 88,
+                                                       'url': 'https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj',
+                                                       'width': 88},
+                                           'high': {'height': 800,
+                                                    'url': 'https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s800-c-k-c0x00ffffff-no-rj',
+                                                    'width': 800},
+                                           'medium': {'height': 240,
+                                                      'url': 'https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s240-c-k-c0x00ffffff-no-rj',
+                                                      'width': 240}},
+                            'title': 'MoscowPython'},
+                'statistics': {'hiddenSubscriberCount': False,
+                               'subscriberCount': '26000',
+                               'videoCount': '686',
+                               'viewCount': '2316562'}}],
+     'kind': 'youtube#channelListResponse',
+     'pageInfo': {'resultsPerPage': 5, 'totalResults': 1}}
+
